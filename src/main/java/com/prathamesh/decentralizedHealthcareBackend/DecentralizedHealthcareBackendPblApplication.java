320c302138b02142293750a849077e41774cf16e
Prathamesh Adate <prathameshadate05@gmail.com>
2023-10-27 07:26:12 +0530

    Project setup

---
+++ src/main/java/com/prathamesh/decentralizedHealthcareBackend/DecentralizedHealthcareBackendPblApplication.java
@@ -0,0 +1,13 @@
+package com.prathamesh.decentralizedHealthcareBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecentralizedHealthcareBackendPblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecentralizedHealthcareBackendPblApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prathamesh/decentralizedHealthcareBackend/DecentralizedHealthcareBackendPblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prathamesh.decentralizedHealthcareBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecentralizedHealthcareBackendPblApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
